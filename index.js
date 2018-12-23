3d16577a17891d2f17ead671c4596f7880b81f10
syan6010 <monday6010@gmail.com>
2018-12-23 20:51:20 +0800

    make it better

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ var bot = linebot({
   channelAccessToken: 'PatWWFB9frLn/SApYvru/vyfmH+vv44D03A1XQOfq5E982ap0ZOdVLA1EwUBKR0bnQm4Ob3zJvEkTdgAFV+cFbNidJyWUpK+SRslvqiONOfMNQgs1Qm61MpLts/6MiIi1EaI9QelogaUzcECqxRU6AdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function(event) {
-  console.log(event); //把收到訊息的 event 印出來看看
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
 });
 
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post('/linewebhook', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
