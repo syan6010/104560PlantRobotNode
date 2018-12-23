3515d1ee275e65cf3b24a0879a02b7b3a4b33f46
syan6010 <monday6010@gmail.com>
2018-12-23 20:41:35 +0800

    make it better

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
+var linebot = require('linebot');
+var express = require('express');
 
-const linebot = require('../index.js');
-const express = require('express');
+var bot = linebot({
+  channelId: '1627664670',
+  channelSecret: '391b4c997a9923bc2e1d484ad2e99fce',
+  channelAccessToken: 'PatWWFB9frLn/SApYvru/vyfmH+vv44D03A1XQOfq5E982ap0ZOdVLA1EwUBKR0bnQm4Ob3zJvEkTdgAFV+cFbNidJyWUpK+SRslvqiONOfMNQgs1Qm61MpLts/6MiIi1EaI9QelogaUzcECqxRU6AdB04t89/1O/w1cDnyilFU='
+});
 
-const bot = linebot({
-  channelId: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
 });
 
 const app = express();
-
 const linebotParser = bot.parser();
+app.post('/', linebotParser);
 
-app.post('/linewebhook', linebotParser);
-
-bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log('Success', data);
-  }).catch(function (error) {
-    console.log('Error', error);
-  });
-});
-
-app.listen(process.env.PORT || 80, function () {
-  console.log('LineBot is running.');
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
