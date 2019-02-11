3f692e75aaf6e3baace1d63cd8e670f5c6f58022
syan6010 <monday6010@gmail.com>
2019-02-11 12:17:41 +0800

    make it better

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ boardReady(board_info, function (board) {
   board.systemReset();
   board.samplingInterval = 50;
   rgbled = getRGBLedCathode(board, 15, 12, 13);
-  rgbled.setColor('#000000');
+  rgbled.setColor('#ffffff');
  
 });
 
