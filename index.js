d94bbfb007e51bb453eb6c2f8fdaa92c26d4b694
syan6010 <monday6010@gmail.com>
2019-02-14 20:50:05 +0800

    make it better

--- index.js
+++ index.js
@@ -64,25 +64,21 @@ bot.on('message', function (event) {
             if(snapshot.exists()) {
                 qAndAStep = snapshot.val();
                 if (qAndAStep === 0 ) {
-                    await event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！！');
+                    event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！！');
                     updateData(lineId, "steps" ,qAndAStep+1);
-                }
-                else if(qAndAStep === 1) {
+                } else if(qAndAStep === 1) {
                     event.reply('可以告訴我你的植物種類嗎？');
                     await updateData(lineId, "deviceId", event.message.text);
                     await updateData(lineId, "steps" ,qAndAStep+1);
-                }
-                else if(qAndAStep === 2) {
+                } else if(qAndAStep === 2) {
                     event.reply('謝謝！我們又邁進了一步！！可以讓我知道要怎麼稱呼你嗎？');
                     await updateData(lineId, "plantType", event.message.text);
                     await updateData(lineId, "steps" ,qAndAStep+1);
-                }
-                else if(qAndAStep === 3) {
+                } else if(qAndAStep === 3) {
                     event.reply('謝謝接下來我們馬上就可以開始使用了！！輸入OK取得資訊');
                     await updateData(lineId, "name", event.message.text);
                     await updateData(lineId, "steps" , 99);
-                }
-                else if(qAndAStep === 99) {
+                } else if(qAndAStep === 99) {
                     switch (event.message.text) {
                         case 'help' :
                             event.reply({
