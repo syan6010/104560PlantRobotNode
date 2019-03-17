9896a968ce48bd949cb797cb9f4933fc7ef7dd8d
syan6010 <monday6010@gmail.com>
2019-03-17 23:20:03 +0800

    make it better

--- index.js
+++ index.js
@@ -94,24 +94,22 @@ bot.on('message', function (event) {
                                 var data = snapshot.val();
                                 switch (data) {
                                     case '薄荷' :
-                                        // return event.reply(['Line 1', {
-                                        //     type: 'template',
-                                        //     altText: 'Buttons alt text',
-                                        //     template: {
-                                        //         type: 'buttons',
-                                        //         thumbnailImageUrl: 'https://i2.kknews.cc/SIG=m8bseq/o0p0008q8qoq4pn7493.jpg',
-                                        //         title: 'My button sample',
-                                        //         text: 'Hello, my button',
-                                        //         actions: [
-                                        //             { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
-                                        //             { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-                                        //             { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                                        //             { label: 'Say message', type: 'message', text: 'Rice=米' },
-                                        //         ],
-                                        //     },
-                                        // }]);
-                                        event.reply('bo ho');
-                                        break;
+                                        return event.reply(['Line 1', {
+                                            type: 'template',
+                                            altText: 'Buttons alt text',
+                                            template: {
+                                                type: 'buttons',
+                                                thumbnailImageUrl: 'https://i2.kknews.cc/SIG=m8bseq/o0p0008q8qoq4pn7493.jpg',
+                                                title: 'My button sample',
+                                                text: 'Hello, my button',
+                                                actions: [
+                                                    { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+                                                    { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                                                    { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                                                    { label: 'Say message', type: 'message', text: 'Rice=米' },
+                                                ],
+                                            },
+                                        }]);
                                     default :
                                         event.reply('這種植物我建議你還是別種了');
                                 }
