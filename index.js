f81178e9d3e897645919e676e5324e3f7feae1f6
syan6010 <monday6010@gmail.com>
2019-03-17 23:17:57 +0800

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('webduino-js');
-require('webduino-blockly');
+// require('webduino-js');
+// require('webduino-blockly');
 
 var linebot = require('linebot');
 var express = require('express');
@@ -24,10 +24,10 @@ var bot = linebot({
   channelAccessToken: 'PatWWFB9frLn/SApYvru/vyfmH+vv44D03A1XQOfq5E982ap0ZOdVLA1EwUBKR0bnQm4Ob3zJvEkTdgAFV+cFbNidJyWUpK+SRslvqiONOfMNQgs1Qm61MpLts/6MiIi1EaI9QelogaUzcECqxRU6AdB04t89/1O/w1cDnyilFU='
 });
 
-var myBoard;
-var rgbled;
+// var myBoard;
+// var rgbled;
 
-var myBoardVars={board: 'Smart', device: '10Q4LapQ', transport: 'mqtt'};
+// var myBoardVars={board: 'Smart', device: '10Q4LapQ', transport: 'mqtt'};
 
 
 
@@ -38,25 +38,25 @@ app.post('/linewebhook', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
     console.log("App now running on port", port);
-  });
+});
   
 
-var users=[];
-var totalSteps=3;
-let deviceId;
-let name;
-let plantType;
+// var users=[];
+// var totalSteps=3;
+// let deviceId;
+// let name;
+// let plantType;
 let lineId;
 var qAndAStep;
-let textFromUser;
+// let textFromUser;
 
-boardReady(myBoardVars, true, function (board) {
-    myBoard=board;
-    board.systemReset();
-    board.samplingInterval = 50;
-    rgbled = getRGBLedCathode(board, 15, 12, 13);
-    rgbled.setColor('#000000');
-});
+// boardReady(myBoardVars, true, function (board) {
+//     myBoard=board;
+//     board.systemReset();
+//     board.samplingInterval = 50;
+//     rgbled = getRGBLedCathode(board, 15, 12, 13);
+//     rgbled.setColor('#000000');
+// });
 
 
 
@@ -82,35 +82,36 @@ bot.on('message', function (event) {
                     updateData(lineId, "name", event.message.text);
                 } else if(qAndAStep === 99) {
                     switch (event.message.text) {
-                        case 'help' :
-                            event.reply({
-                                type: 'image',
-                                originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
-                                previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
-                            });
-                            break;
+                        // case 'help' :
+                        //     event.reply({
+                        //         type: 'image',
+                        //         originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
+                        //         previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
+                        //     });
+                        //     break;
                         case 'ok' :
                             firebase.database().ref(`users/${lineId}/plantType`).once('value', function (snapshot) {
                                 var data = snapshot.val();
                                 switch (data) {
                                     case '薄荷' :
-                                        return event.reply(['Line 1', {
-                                            type: 'template',
-                                            altText: 'Buttons alt text',
-                                            template: {
-                                                type: 'buttons',
-                                                thumbnailImageUrl: 'https://i2.kknews.cc/SIG=m8bseq/o0p0008q8qoq4pn7493.jpg',
-                                                title: 'My button sample',
-                                                text: 'Hello, my button',
-                                                actions: [
-                                                    { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
-                                                    { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
-                                                    { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                                                    { label: 'Say message', type: 'message', text: 'Rice=米' },
-                                                ],
-                                            },
-                                        }]);
-        
+                                        // return event.reply(['Line 1', {
+                                        //     type: 'template',
+                                        //     altText: 'Buttons alt text',
+                                        //     template: {
+                                        //         type: 'buttons',
+                                        //         thumbnailImageUrl: 'https://i2.kknews.cc/SIG=m8bseq/o0p0008q8qoq4pn7493.jpg',
+                                        //         title: 'My button sample',
+                                        //         text: 'Hello, my button',
+                                        //         actions: [
+                                        //             { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+                                        //             { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                                        //             { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                                        //             { label: 'Say message', type: 'message', text: 'Rice=米' },
+                                        //         ],
+                                        //     },
+                                        // }]);
+                                        event.reply('bo ho');
+                                        break;
                                     default :
                                         event.reply('這種植物我建議你還是別種了');
                                 }
@@ -120,22 +121,22 @@ bot.on('message', function (event) {
                             qAndAStep = -1; 
                             event.reply('ok輸入y開始重新設定');
                             break;
-                        case 'led開' :
-                            if (!deviceIsConnected())
-                                event.reply('裝置未連接');
-                            else{                       
-                                myResult='LED已打開！';
-                                rgbled.setColor('#ffffff');                   
-                            }
-                            break;
-                        case 'led關' :
-                            if (!deviceIsConnected())
-                                event.reply('裝置未連接');
-                            else{
-                                myResult='LED已關閉！';
-                                rgbled.setColor('#000000');
-                            }
-                            break;
+                        // case 'led開' :
+                        //     if (!deviceIsConnected())
+                        //         event.reply('裝置未連接');
+                        //     else{                       
+                        //         myResult='LED已打開！';
+                        //         rgbled.setColor('#ffffff');                   
+                        //     }
+                        //     break;
+                        // case 'led關' :
+                        //     if (!deviceIsConnected())
+                        //         event.reply('裝置未連接');
+                        //     else{
+                        //         myResult='LED已關閉！';
+                        //         rgbled.setColor('#000000');
+                        //     }
+                        //     break;
                         default:
                             event.reply('我不能這麼做!!');
                       }
@@ -169,16 +170,16 @@ bot.on('message', function (event) {
 
 
 
-function deviceIsConnected(){
-    if (myBoard===undefined){
-        return 'false1';
-    }
-    else if (myBoard.isConnected===undefined) {
-        return 'false2';
-    }
-    else
-       return myBoard.isConnected;
- }
+// function deviceIsConnected(){
+//     if (myBoard===undefined){
+//         return 'false1';
+//     }
+//     else if (myBoard.isConnected===undefined) {
+//         return 'false2';
+//     }
+//     else
+//        return myBoard.isConnected;
+//  }
 
  let updateData = (lineId, postKey, postData) => {
       let updates = {};
